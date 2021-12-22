9b0fae0a6f4257697847adb227a46e235cd5b7b1
ym <yael.montufar@protonmail.com>
2021-12-21 23:57:32 -0600

    Test Hermes

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
+//@ts-nocheck
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const isHermes = JSON.stringify(!!global.HermesInternal)
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{isHermes}</Text>
       <StatusBar style="auto" />
     </View>
   );
